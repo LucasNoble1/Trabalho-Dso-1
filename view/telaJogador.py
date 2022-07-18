9ebef38dff338296ffaf5d26f9dbc523026df566
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2022-07-18 16:21:01 -0300

    Update telaJogador.py

--- view/telaJogador.py
+++ view/telaJogador.py
@@ -1,5 +1,6 @@
 from view.telaAbstract import Tela
 #from controller.controladorJogador import ControladorJogador
+import PySimpleGUI as sg
 
 class TelaJogador(Tela):
     def __init__(self, controlador):
@@ -34,3 +35,35 @@ class TelaJogador(Tela):
         print("\n")
         opcao = self.le_num_inteiro("Selecione uma opção:", [1, 2, 3, 4, 0])
         return opcao
+
+    def tela_pergunta(self):
+        self.init_components()
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['4']:
+            opcao = 4
+        # cobre os casos de voltar, não clicar em nada e fechar janela
+        if values['0'] or button in (None, 'Cancelar'):
+            opcao = 0
+        self.close()
+        return opcao
+
+        # metodo para iniciar os componentes da interface grafica da tela de Pergunta
+    def init_components(self):
+        sg.ChangeLookAndFeel('Reddit')
+        layout = [
+            [sg.Text('-----Jogador-----')]
+            [sg.Text('--------------------------')]
+            [sg.Radio('1 - Cadastrar Jogador', "RD1", key='1')],
+            [sg.Radio('2 - Editar um Jogador', "RD1", key='2')],
+            [sg.Radio('3 - Excluir um jogador', "RD1", key='3')],
+            [sg.Radio('4 - Pontuçao dos jogadores', "RD1", key='4')],
+            [sg.Cancel('0 - Voltar')]
+            ]
+        self.__window = sg.Window('Jogador').Layout(layout)
