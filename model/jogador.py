fc73ab16646f6b3fbef47e834fec4d878af72221
LucasNoble1 <56458675+LucasNoble1@users.noreply.github.com>
2022-07-22 19:20:19 -0300

    Update jogador.py

--- model/jogador.py
+++ model/jogador.py
@@ -1,23 +1,31 @@
 
 
 class Jogador():
-    def __init__(self,nome: str):
+    def __init__(self,nome: str, cpf: str):
         self.__nome = nome
+        self.__cpf = cpf
         self.__acertos = 0
         self.__erros = 0
+        self.__jogou = False
+
+    @property
+    def cpf(self):
+        return self.__cpf
 
     #calcula os pontos
     def pontos(self):
-        pontos = self.__acertos - self.__erros
+        e = self.__erros
+        a = self.__acertos
+        pontos = a - e
         return pontos
 
     #aumento os acertos
     def acertou(self):
-        self.__acertos =+ 1
+        self.__acertos = self.__acertos + 1
 
     #diminui os erros
     def errou(self):
-        self.__erros =- 1
+        self.__erros = self.__erros + 1
 
     @property
     def nome(self):
@@ -35,3 +43,10 @@ class Jogador():
     def nome(self, nome):
         self.__nome = nome
 
+    @property
+    def jogou(self):
+        return self.__jogou
+
+    @jogou.setter
+    def jogou(self, condicao):
+        self.__jogou = condicao
