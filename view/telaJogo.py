e5926bfcfc94d4a33ad16381032da009a63d365a
LucasNoble1 <56458675+LucasNoble1@users.noreply.github.com>
2022-07-24 16:01:58 -0300

    Update telaJogo.py

--- view/telaJogo.py
+++ view/telaJogo.py
@@ -1,13 +1,59 @@
 from view.telaAbstract import Tela
 import PySimpleGUI as sg
-#from controller.controladorJogo import ControladorJogo
 
 class TelaJogo(Tela):
     def __init__(self, controlador):
         super().__init__()
-        #if isinstance(controlador):
         self.__controlador = controlador
         self.__window = None
+
+    def tela_passo_um(self):
+        print("\n")
+        print("Passo 1: Escolha a quantidade de jogadores")
+        print("------------------------------------------")
+        print("1 -- MODO SOLO")
+        print("2 -- MODO BORA X1")
+        print("3 -- MODO CADA UM POR SI")
+        print("\n")
+        num_jogadores = self.le_num_inteiro("Selecione uma opção:", [1, 2, 3])
+        print("Passo 2: Escolha a quantidade de turnos")
+        print("------------------------------------------")
+        print("1 -- iniciar um jogo com >3< TURNOS")
+        print("2 -- iniciar um jogo com >5< TURNOS")
+        print("3 -- iniciar um jogo com >7< TURNOS")
+        print("0 - VOLTAR PARA A TELA PRINCIPAL")
+        print("\n")
+        num_turnos = self.le_num_inteiro("Selecione uma opção:", [1, 2, 3, 0])
+        return num_jogadores , num_turnos
+
+    def selecao_jogadores(self):
+        print("1 -- Criar novo jogador")
+        print("2 -- Selecionar Jogador")
+        resposta = self.le_num_inteiro("selecione uma opção:", [1, 2])
+        return resposta
+
+    def vez_do_jogador(self, nome):
+        print("========================")
+        print("Vez do jogador : ", nome)
+        print("QUE A FORÇA ESTEJA COM VOCÊ!!!")
+        print("=============================")
+        ok = self.le_num_inteiro("DIGITE 1 para iniciar:", [1])
+        return ok
+
+    def tela_fim(self):
+        print("\n")
+        print("====================")
+        print(">>>FIM DO JOGO<<<")
+
+
+    def pontuacao_jogo(self, nome, acertos, erros, pontos):
+        print("Confira a pontuação")
+        print("Jogador: ", nome)
+        print("acertos: ", acertos)
+        print("erros: ", erros)
+        print("pontuação: ", pontos)
+        print("----------------------")
+
 
     def tela_passo_um(self):
         self.init_components()
@@ -37,5 +83,4 @@ class TelaJogo(Tela):
         ]
         self.__window = sg.Window('Passo 1').Layout(layout)
 
-    #só usamos os metodos abstratos nela
-    #GENIAL NE? nota 10
+  
