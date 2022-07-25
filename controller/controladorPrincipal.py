302648907fa95ca5a2516e453f38d2da5843fe4d
LucasNoble1 <56458675+LucasNoble1@users.noreply.github.com>
2022-07-25 17:26:03 -0300

    Update controladorPrincipal.py

--- controller/controladorPrincipal.py
+++ controller/controladorPrincipal.py
@@ -2,13 +2,13 @@ from controller.controladorJogo import ControladorJogo
 from controller.controladorJogador import ControladorJogador
 from controller.controladorPergunta import ControladorPergunta
 from view.telaPrincipal import TelaPrincipal
-from model.Pergunta import Pergunta
 
 
 class ControladorPrincipal:
     def __init__(self):
+        #self.__instance = None
         self.__controlador_jogo = ControladorJogo(self)
-        self.__controlador_jogador = ControladorJogador(self)
+        self.__controlador_jogador =ControladorJogador(self)
         self.__controlador_pergunta = ControladorPergunta(self)
         self.__tela_principal = TelaPrincipal(self)
 
@@ -27,7 +27,7 @@ class ControladorPrincipal:
 
     #abre a tela de opcoes do sistema
     def inicializa_sistema(self):
-        lista_opcoes = {1: self.iniciar_jogo, 2: self.adicionar_pergunta, 3: self.tutorial}
+        lista_opcoes = {0: self.encerrar_sistema ,1: self.iniciar_jogo, 2: self.pergunta_opcoes, 3: self.jogador_opcoes, 4 : self.tutorial}
 
         continua = True
         while continua:
@@ -35,17 +35,22 @@ class ControladorPrincipal:
 
     #inicia o controladorjogo
     def iniciar_jogo(self):
-        self.__controlador_jogo.passo_um()
+        self.__controlador_jogo.inicia()
     #inicia as rodadas de perguntas
     def rodada_jogo(self):
         self.__controlador_pergunta.mostrar_pergunta()
 
     #sera implementado mais tarde
-    def adicionar_pergunta(self):
+    def pergunta_opcoes(self):
         self.__controlador_pergunta.abre_tela()
 
-    #mostra o tutorial... que ainda n foi implementado
+    def jogador_opcoes(self):
+        self.__controlador_jogador.abre_tela()
+
     def tutorial(self):
         self.__tela_principal.tutorial()
+
+    def encerrar_sistema(self):
+        exit(0)
 
 
