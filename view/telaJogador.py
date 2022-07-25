f8110ab207e0cd939f7c3896162c4f150b0d9650
LucasNoble1 <56458675+LucasNoble1@users.noreply.github.com>
2022-07-25 17:31:51 -0300

    Update telaJogador.py

--- view/telaJogador.py
+++ view/telaJogador.py
@@ -1,73 +1,15 @@
 from view.telaAbstract import Tela
+import PySimpleGUI as sg
 
 class TelaJogador(Tela):
     def __init__(self, controlador):
         super().__init__()
         self.__controlador = controlador
 
-    def selecionar_jogador(self):
-        cpf = input("digite o cpf de um jogador:")
-        return cpf
-    def novo_nome(self):
-        nome = input("Digite o novo nome do jogador:")
-        return nome
 
-    #armazena o nome do jogador
-    def novo_jogador(self , mensagem : str ):
-        while True:
-            print("==========" , mensagem , "==============")
-            nome = input("Digite o nome do jogador:")
-            cpf = input("Digite seu cpf:")
-            if nome == "" or cpf == "":
-                print("AVISO: NOME e CPF não podem ser deixados em branco")
-            else:
-                return nome , cpf
 
-    #mostra nome e pontuação do jogador
-    def mostrar_jogador(self, nome, pontos, cpf ):
-        print("cpf: ", cpf , " ===>Jogador:", nome)
-        print("Pontucão do jogador:", pontos)
-        print("-------------------------")
-
-    def ranking(self, posicao, nome, acertos, erros, pontos, jogos, media):
-        print("===== TOP ", posicao, " =====")
-        print("=> Nome: ", nome)
-        print("=> Total de acertos: ", acertos)
-        print("=> total de erros: ", erros)
-        print("=> Total de pontos: ", pontos)
-        print("=> Total de jogos: ", jogos)
-        print("=> Media de pontos por partida: ", media)
-
-    #opcoes do jogador
-    #essa função foi cortada do trabalho
     def tela_opcoes(self):
-        print("-----Opçoes-----")
-        print("\n")
-        print("1 - Cadastrar Jogador")
-        print("2 - Editar  o nome de um Jogador")
-        print("3 - Excluir um jogador")
-        print("4 - Ranking dos jogadores ")
-        print("0 - voltar")
-        print("\n")
-        opcao = self.le_num_inteiro("Selecione uma opção:", [1, 2, 3, 4, 0])
-        return opcao
-
-    def aviso_jogador_removido(self, nome):
-        print('o jogador :', nome ,'foi removido do sistema')
-
-    def aviso_jogador_nao_encontrado(self,cpf):
-        print('o cpf: ', cpf ,'não corresponde a nenhum jogador')
-        print('selecione um cpf valido')
-
-    def aviso_jogador_duplicado(self):
-        print("Impossivel adicionar jogador")
-        print("Cpf ou Nome indisponiveis")
-    def aviso_jogador_alterado(self):
-        print("O nome do jogador foi alterado com sucesso")
-       
-
-    def tela_pergunta(self):
-        self.init_components()
+        self.init_opcoes()
         button, values = self.__window.Read()
         opcao = 0
         if values['1']:
@@ -78,22 +20,110 @@ class TelaJogador(Tela):
             opcao = 3
         if values['4']:
             opcao = 4
-        # cobre os casos de voltar, não clicar em nada e fechar janela
         if values['0'] or button in (None, 'Cancelar'):
             opcao = 0
         self.close()
         return opcao
 
-        # metodo para iniciar os componentes da interface grafica da tela de Pergunta
-    def init_components(self):
+    def init_opcoes(self):
         sg.ChangeLookAndFeel('Reddit')
         layout = [
-            [sg.Text('-----Jogador-----')]
-            [sg.Text('--------------------------')]
-            [sg.Radio('1 - Cadastrar Jogador', "RD1", key='1')],
-            [sg.Radio('2 - Editar um Jogador', "RD1", key='2')],
-            [sg.Radio('3 - Excluir um jogador', "RD1", key='3')],
-            [sg.Radio('4 - Pontuçao dos jogadores', "RD1", key='4')],
-            [sg.Cancel('0 - Voltar')]
-            ]
-        self.__window = sg.Window('Jogador').Layout(layout)
+            [sg.Text('Jogadores')],
+            [sg.Text('_' * 40)],
+            [sg.Radio('Cadastrar um jogador', "RD1", key='1')],
+            [sg.Radio('Editar nome de um jogador', "RD1", key='2')],
+            [sg.Radio('Excluir jogador', "RD1", key='3')],
+            [sg.Radio('RANKING', "RD1", key='4')],
+            [sg.Radio('Voltar', "RD1", key='0')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Opções').Layout(layout)
+
+
+    def selecionar_jogador(self):
+        sg.ChangeLookAndFeel('Reddit')
+        layout = [
+            [sg.Text('--- Digite o CPF do jogador ---', font=("Helvica", 20))],
+            [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Selecione um jogador').Layout(layout)
+
+        button, values = self.open()
+        cpf = values['cpf']
+        self.close()
+        return cpf
+
+    def novo_nome(self):
+        sg.ChangeLookAndFeel('Reddit')
+        layout = [
+            [sg.Text('--- Digite o NOVO NOME do jogador ---', font=("Helvica", 20))],
+            [sg.Text('NOME:', size=(15, 1)), sg.InputText('', key='nome')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Alterar Nome').Layout(layout)
+
+        button, values = self.open()
+        nome = values['nome']
+        self.close()
+        return nome
+
+
+    def novo_jogador(self):
+        sg.ChangeLookAndFeel('Reddit')
+        layout = [
+            [sg.Text('--- Criar Jogador---', font=("Helvica", 15))],
+            [sg.Text('Nome:', size=(15, 1)), sg.InputText('', key='nome')],
+            [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+
+        ]
+        self.__window = sg.Window('Novo Jogador').Layout(layout)
+
+        button, values = self.open()
+        nome = values['nome']
+        cpf = values['cpf']
+        self.close()
+        if nome == '' or cpf == '':
+            self.mostra_mensagem('AVISO:NOME e CPF não podem ser deixados em branco')
+        else:
+            return nome, cpf
+
+    def mostrar_jogadores(self, dados_jogadores):
+        string_todos_jogadores = ""
+        for dado in dados_jogadores:
+            string_todos_jogadores = string_todos_jogadores + "JOGADOR: " + dado["nome"] + '\n'
+            string_todos_jogadores = string_todos_jogadores + "CPF: " + str(dado["cpf"]) + '\n\n'
+
+
+        sg.Popup('Lista de JOGADORES', string_todos_jogadores)
+
+    def ranking(self, posicao, nome, acertos, erros, pontos, jogos, media):
+        print("===== TOP ", posicao, " =====")
+        print("=> Nome: ", nome)
+        print("=> Total de acertos: ", acertos)
+        print("=> total de erros: ", erros)
+        print("=> Total de pontos: ", pontos)
+        print("=> Total de jogos: ", jogos)
+        print("=> Media de pontos por partida: ", media)
+
+    def ranking2(self, dados_jogadores):
+        string_todos_jogadores = ""
+        for dado in dados_jogadores:
+            string_todos_jogadores = string_todos_jogadores + "COLOCAÇÃO: " + dado["posicao"] + '\n'
+            string_todos_jogadores = string_todos_jogadores + "JOGADOR: " + str(dado["nome"]) + '\n'
+            string_todos_jogadores = string_todos_jogadores + "Numero de acertos: " + dado["acertos"] + '\n'
+            string_todos_jogadores = string_todos_jogadores + "Numero de erros: " + dado["erros"] + '\n'
+            string_todos_jogadores = string_todos_jogadores + "Numero de pontos: " + dado["pontos"] + '\n'
+            string_todos_jogadores = string_todos_jogadores + "Numero de jogos: " + dado["jogos"] + '\n'
+            string_todos_jogadores = string_todos_jogadores + "Media de pontos por jogo: " + dado["media"] + '\n\n'
+
+        sg.Popup('-------- Ranking dos JOGADORES ----------', string_todos_jogadores)
+
+    def close(self):
+        self.__window.Close()
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
+
