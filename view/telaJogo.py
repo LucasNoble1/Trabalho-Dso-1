2a7b685fe9543586eff851db84e3b098ce191247
LucasNoble1 <56458675+LucasNoble1@users.noreply.github.com>
2022-07-25 17:33:20 -0300

    Update telaJogo.py

--- view/telaJogo.py
+++ view/telaJogo.py
@@ -7,56 +7,42 @@ class TelaJogo(Tela):
         self.__controlador = controlador
         self.__window = None
 
-    def tela_passo_um(self):
-        print("\n")
-        print("Passo 1: Escolha a quantidade de jogadores")
-        print("------------------------------------------")
-        print("1 -- MODO SOLO")
-        print("2 -- MODO BORA X1")
-        print("3 -- MODO CADA UM POR SI")
-        print("\n")
-        num_jogadores = self.le_num_inteiro("Selecione uma opção:", [1, 2, 3])
-        print("Passo 2: Escolha a quantidade de turnos")
-        print("------------------------------------------")
-        print("1 -- iniciar um jogo com >3< TURNOS")
-        print("2 -- iniciar um jogo com >5< TURNOS")
-        print("3 -- iniciar um jogo com >7< TURNOS")
-        print("0 - VOLTAR PARA A TELA PRINCIPAL")
-        print("\n")
-        num_turnos = self.le_num_inteiro("Selecione uma opção:", [1, 2, 3, 0])
-        return num_jogadores , num_turnos
+    def vez_do_jogador(self, nome):
+        sg.popup("Vez do jogador: ", nome )
+
+    def pontuacao_jogo(self, dados_jogadores):
+        string_todos_jogadores = ""
+        for dado in dados_jogadores:
+            string_todos_jogadores = string_todos_jogadores + "JOGADOR: " + dado["nome"] + '\n'
+            string_todos_jogadores = string_todos_jogadores + "Numero de acertos: " + dado["acertos"] + '\n'
+            string_todos_jogadores = string_todos_jogadores + "Numero de erros: " + dado["erros"] + '\n'
+            string_todos_jogadores = string_todos_jogadores + "Numero de pontos: " + dado["pontos"] + '\n\n'
+
+        sg.Popup('---Pontuação da Partida---', string_todos_jogadores)
 
     def selecao_jogadores(self):
-        print("1 -- Criar novo jogador")
-        print("2 -- Selecionar Jogador")
-        resposta = self.le_num_inteiro("selecione uma opção:", [1, 2])
-        return resposta
+        sg.ChangeLookAndFeel('Reddit')
+        layout = [
+            [sg.Text('_' * 40)],
+            [sg.Radio('Criar um novo jogador', "RD1", key='1')],
+            [sg.Radio('Selecionar Jogador Existente', "RD1", key='2')],
+            [sg.Button('Confirmar')]
+        ]
+        self.__window = sg.Window('Opções de jogadores').Layout(layout)
+        self.open()
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        self.close()
+        return opcao
 
-    def vez_do_jogador(self, nome):
-        print("========================")
-        print("Vez do jogador : ", nome)
-        print("QUE A FORÇA ESTEJA COM VOCÊ!!!")
-        print("=============================")
-        ok = self.le_num_inteiro("DIGITE 1 para iniciar:", [1])
-        return ok
-
-    def tela_fim(self):
-        print("\n")
-        print("====================")
-        print(">>>FIM DO JOGO<<<")
-
-
-    def pontuacao_jogo(self, nome, acertos, erros, pontos):
-        print("Confira a pontuação")
-        print("Jogador: ", nome)
-        print("acertos: ", acertos)
-        print("erros: ", erros)
-        print("pontuação: ", pontos)
-        print("----------------------")
 
 
     def tela_passo_um(self):
-        self.init_components()
+        self.init_passo_um()
         button, values = self.__window.Read()
         opcao = 0
         if values['1']:
@@ -65,13 +51,12 @@ class TelaJogo(Tela):
             opcao = 2
         if values['3']:
             opcao = 3
-        # cobre os casos de voltar, não clicar em nada e fechar janela
-        if values['0'] or button in (None, 'Cancelar'):
+        if button in (None, 'Cancelar'):
             opcao = 0
         self.close()
         return opcao
 
-    def init_components(self):
+    def init_passo_um(self):
         sg.ChangeLookAndFeel('Reddit')
         layout = [
             [sg.Text('Escolha a quantidade de jogadores')],
@@ -79,8 +64,43 @@ class TelaJogo(Tela):
             [sg.Radio('1 jogador(MODO SOLO)', "RD1", key='1')],
             [sg.Radio('2 jogadores(MODO X1)', "RD1", key='2')],
             [sg.Radio('3 jogadores(MODO CADA UM POR SI', "RD1", key='3')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Numero de jogadores').Layout(layout)
+
+    def tela_passo_dois(self):
+        self.init_passo_dois()
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 3
+        if values['2']:
+            opcao = 5
+        if values['3']:
+            opcao = 7
+        if button in (None, 'Cancelar'):
+            opcao = 0
+        self.close()
+        return opcao
+
+    def init_passo_dois(self):
+        sg.ChangeLookAndFeel('Reddit')
+        layout = [
+            [sg.Text('Escolha a quantidade de turnos')],
+            [sg.Text('_' * 40)],
+            [sg.Radio('3 Turnos', "RD1", key='1')],
+            [sg.Radio('5 Turnos', "RD1", key='2')],
+            [sg.Radio('7 Turnos', "RD1", key='3')],
             [sg.Button('Confirmar'), sg.Cancel('voltar')]
         ]
-        self.__window = sg.Window('Passo 1').Layout(layout)
+        self.__window = sg.Window('Numero de turnos').Layout(layout)
+
+    def close(self):
+        self.__window.Close()
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
+
 
   
