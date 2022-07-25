95f3868a22e098105ecfaf5b5a15dd5adf9d091f
LucasNoble1 <56458675+LucasNoble1@users.noreply.github.com>
2022-07-25 17:17:19 -0300

    Update dao.py

--- DAOs/dao.py
+++ DAOs/dao.py
@@ -1,11 +1,11 @@
-import pickle
 from abc import ABC, abstractmethod
 
 class DAO(ABC):
     @abstractmethod
     def __init__(self, datasource=''):
         self.__datasource = datasource
-        self.__cache = {} #é aqui que vai ficar a lista que estava no controlador. Nesse exemplo estamos usando um dicionario
+        self.__cache = {}
+        self.__cache2 = []
         try:
             self.__load()
         except FileNotFoundError:
@@ -17,33 +17,28 @@ class DAO(ABC):
     def __load(self):
         self.__cache = pickle.load(open(self.__datasource,'rb'))
 
-    #esse método precisa chamar o self.__dump()
-    def add(self, key, obj):
-        self.__cache[key] = obj
-        self.__dump()  #atualiza o arquivo depois de add novo amigo
+    def add(self, obj):
+        self.__cache2.append(obj)
+        self.__dump()
 
-    #cuidado: esse update só funciona se o objeto com essa chave já existe
-    def update(self, key, obj):
-        try:
-            if(self.__cache[key] != None):
-                self.__cache[key] = obj #atualiza a entrada
-                self.__dump()  #atualiza o arquivo
-        except KeyError:
-            pass  # implementar aqui o tratamento da exceção
+    def update(self, obj):
+        for i in self.get_all():
+            if i.codigo == obj.codigo:
+                self.__cache2.remove(i)
+                self.__cache2.append(obj)
+                self.__dump()
 
     def get(self, key):
-        try:
-            return self.__cache[key]
-        except KeyError:
-            pass #implementar aqui o tratamento da exceção
+        for i in self.get_all():
+            if i.codigo == key:
+                return i
+        return None
 
-    # esse método precisa chamar o self.__dump()
     def remove(self, key):
-        try:
-            self.__cache.pop(key)
-            self.__dump() #atualiza o arquivo depois de remover um objeto
-        except KeyError:
-            pass #implementar aqui o tratamento da exceção
+        for i in self.get_all():
+            if i.codigo == key:
+                self.__cache2.remove(i)
+                self.__dump()
 
     def get_all(self):
-        return self.__cache.values()
+        return self.__cache2
