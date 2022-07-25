cc9efbc0e6d6b841bb28860d12cc6382ddbe30dd
LucasNoble1 <56458675+LucasNoble1@users.noreply.github.com>
2022-07-25 17:34:45 -0300

    Update telaPergunta.py

--- view/telaPergunta.py
+++ view/telaPergunta.py
@@ -7,23 +7,9 @@ class TelaPergunta(Tela):
         self.__controlador = controlador
         self.__window = None
 
-
-    def tela_opcoes(self):
-        print("\n")
-        print("-----Opçoes-----")
-        print("\n")
-        print("1 - Criar uma Nova pergunta")
-        print("2 - Alterar uma pergunta")
-        print("3 - Excluir uma pergunta")
-        print("0 - voltar")
-        print("\n")
-        opcao = self.le_num_inteiro("Selecione uma opção:", [1, 2, 3, 0])
-        return opcao
-
-    #metodo que mostra a pergunta e suas alternativas durante o jogo
     def mostrar_pergunta(self, pergunta, alt1, alt2, alt3, nome):
         print("=============================")
-        print("jogador da vez: ",nome)
+        print("jogador da vez: ", nome)
         print("          PERGUNTA:         ")
         print(">", pergunta, "<")
         print("=============================")
@@ -32,105 +18,11 @@ class TelaPergunta(Tela):
         print("2 --", alt2)
         print("3 --", alt3)
         opcao = self.le_num_inteiro("Selecione uma opção:", [1, 2, 3])
-
         return opcao
-    def criar_pergunta(self):
-        print("============================")
-        print("=== CRIE SUA PERGUNTA!! :)===")
-        print("============================")
-        pergunta = input("Qual a Pergunta?")
-        print("============================")
-        print("Sua pergunta vai precisar de uma alternativa correta e duas erras")
-        print("===========================")
-        print("Pergunta:", pergunta)
-        resposta = input("Informe a resposta correta de sua pergunta:")
-        alternativa1 = input("Crie uma alternativa falsa para sua pergunta:")
-        alternativa2 = input("Crie outra alternativa falsa para sua pergunta:")
-        id = input("Crie um ID para a pergunta")
-
-        return pergunta, resposta, alternativa1, alternativa2, id
-    #informa q a pergunta ja existe.
-    #mostra a pergunta
-    #volta para a tela inicial de perguntas
-    def pergunta_repetida(self,pergunta):
-        print("==")
-        print("ERRO!")
-        print(pergunta + "JA EXISTE NO SISTEMA")
-        self.__controlador.abre_tela()
-
-    #mensagem informando que a pergunta foi adicionada
-    #volta para a tela inicial de perguntas
-    def pergunta_adicionada(self,pergunta):
-        print("A Pergunta:" + pergunta)
-        print("FOI ADICIONADA COM SUCESSO!!!")
-        self.__controlador.abre_tela()
-
-    def mostrar_todas_perguntas(self, codigo, pergunta):
-        print(codigo, "====>" , pergunta)
-
-    def editar_pergunta(self, p, r, a1, a2):
-        pergunta = p
-        resposta = r
-        alternativa1 = a1
-        alternativa2 = a2
-        print("==Editar uma pergunta==")
-        print(" 1 => PERGUNTA: ", pergunta)
-        print(" 2 => RESPOSTA: ", resposta)
-        print(" 3 => ALTERNATIVA 1: ", alternativa1)
-        print(" 4 => ALTERNATIVA 2: ", alternativa2)
-        print(" 0 => VOLTAR")
-        opcao = self.le_num_inteiro("Selecione a opção que deseja alterar:", [0, 1, 2, 3, 4])
-        if opcao == 0:
-            return "0", 0
-        elif opcao == 1:
-            print("PERGUNTA ANTIGA: ", pergunta)
-            pergunta_nova = str(input("Digite a nova pergunta:"))
-            print("Suas alteracoes foram salvas com sucesso!!!")
-            print("=========================================")
-            return pergunta_nova , opcao
-        elif opcao == 2:
-            print("RESPOSTA ANTIGA: ", resposta)
-            resposta = str(input("Digite a nova resposta:"))
-            print("Suas alteracoes foram salvas com sucesso!!!")
-            print("=========================================")
-            return resposta, opcao
-        elif opcao == 3:
-            print("ALTERNATIVA ANTIGA: ", alternativa1)
-            resposta = str(input("Digite a nova alternativa:"))
-            print("Suas alteracoes foram salvas com sucesso!!!")
-            print("=========================================")
-            return resposta, opcao
-        elif opcao == 4:
-            print("ALTERNATIVA ANTIGA: ", alternativa2)
-            resposta = str(input("Digite a nova alternativa:"))
-            print("Suas alteracoes foram salvas com sucesso!!!")
-            print("=========================================")
-            return resposta, opcao
-
-
-    def pedir_pergunta(self):
-        print("===============================================")
-        print('\n')
-        codigo = input("Digite o codigo da pergunta:")
-        return codigo
-
-    def pergunta_removida(self, pergunta):
-        print("A Pergunta:" + pergunta)
-        print("FOI REMOVIDA COM SUCESSO!!!")
-        self.__controlador.abre_tela()
-
-    def pergunta_inexistente(self, codigo):
-        codigo = str(codigo)
-        print("\n")
-        print("ERRO")
-        print("O codigo ", codigo , "nao coresponde a nenhuma pergunta!")
-        print("Nenhuma pergunta foi excluida, ou alterada!")
-        self.tela_opcoes()
-
- 
-        # tela pergunta interf grafica
-    def tela_pergunta_teste(self):
-        self.init_components()
+    
+    # metodo que mostra a pergunta e suas alternativas durante o jogo
+    def mostrar_pergunta2(self, pergunta, alt1, alt2, alt3, nome):
+        self.init_pergunta(pergunta, alt1, alt2, alt3, nome)
         button, values = self.__window.Read()
         opcao = 0
         if values['1']:
@@ -139,21 +31,187 @@ class TelaPergunta(Tela):
             opcao = 2
         if values['3']:
             opcao = 3
-        # cobre os casos de voltar, não clicar em nada e fechar janela
-        if values['0'] or button in (None, 'Cancelar'):
+        self.close()
+        return opcao
+
+    def init_pergunta(self, pergunta, alt1, alt2, alt3, nome):
+        sg.ChangeLookAndFeel('Reddit')
+        layout = [
+            [sg.Text('Jogador da vez:', nome)],
+            [sg.Text('---------PERGUNTA:----------')],
+            [sg.Text(pergunta, font=("Helvica", 15))],
+            [sg.Text('_' * 40)],
+            [sg.Radio(alt1, "RD1", key='1')],
+            [sg.Radio(alt2, "RD1", key='2')],
+            [sg.Radio(alt3, "RD1", key='3')],
+            [sg.Button('Confirmar')]
+        ]
+        self.__window = sg.Window('Opções: JOGADORES').Layout(layout)
+    def tela_opcoes(self):
+        self.init_opcoes()
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if button in (None, 'Cancelar'):
             opcao = 0
         self.close()
         return opcao
-     
-    # metodo para iniciar os componentes da interface grafica da tela de Pergunta
-    def init_components(self):
+
+    def init_opcoes(self):
         sg.ChangeLookAndFeel('Reddit')
         layout = [
-            [sg.Text('-----Opçoes-----')]
-            [sg.Text('--------------------------')]
-            [sg.Radio('1 - Cadastrar pergunta', "RD1", key='1')],
-            [sg.Radio('2 - Excluir pergunta', "RD1", key='2')],
-            [sg.Radio('3 - Editar pergunta', "RD1", key='3')],
-            [sg.Cancel('0 - voltar')]
+            [sg.Text('PERGUNTAS')],
+            [sg.Text('_' * 40)],
+            [sg.Radio('Criar uma nova pergunta', "RD1", key='1')],
+            [sg.Radio('Editar uma pergunta', "RD1", key='2')],
+            [sg.Radio('Excluir uma pergunta', "RD1", key='3')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Opções').Layout(layout)
+
+    def criar_pergunta(self):
+        sg.ChangeLookAndFeel('Reddit')
+        layout = [
+            [sg.Text('-- Criar nova Pergunta --', font=("Helvica", 15))],
+            [sg.Text('Pergunta: ', size=(15, 1)), sg.InputText('', key='pergunta')],
+            [sg.Text('Resposta: ', size=(15, 1)), sg.InputText('', key='resposta')],
+            [sg.Text('Alternativa 1: ', size=(15, 1)), sg.InputText('', key='alternativa1')],
+            [sg.Text('Alternativa 2:', size=(15, 1)), sg.InputText('', key='alternativa2')],
+            [sg.Text('codigo:', size=(15, 1)), sg.InputText('', key='codigo')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+
+        ]
+        self.__window = sg.Window('Adicionar Pergunta').Layout(layout)
+
+        button, values = self.open()
+        pergunta = values['pergunta']
+        resposta = values['resposta']
+        alternativa1 = values['alternativa1']
+        alternativa2 = values['alternativa2']
+        codigo = values['codigo']
+        self.close()
+        if pergunta == '' or resposta == ''or alternativa1 == ''or alternativa2 == ''or codigo == '' or button in (None, 'Cancelar') :
+            self.mostra_mensagem('AVISO:nenhuma das categorias podem ser deixadas em branco')
+            return 0, 0, 0, 0, 0
+        else:
+            return pergunta, resposta, alternativa1, alternativa2, codigo
+
+    def mostrar_todas_perguntas(self,dados_perguntas):
+        string_todos_perguntas = ""
+        for dado in dados_perguntas:
+            string_todos_perguntas = string_todos_perguntas + dado["codigo"] + "===> " + dado["pergunta"] + '\n'
+
+        sg.Popup('Lista de Perguntas', string_todos_perguntas)
+
+    def editar_pergunta(self, pergunta, resposta, alternativa1, alternativa2):
+        sg.ChangeLookAndFeel('Reddit')
+        layout = [
+            [sg.Text('Selecione a opção que deseja alterar')],
+            [sg.Text('_' * 40)],
+            [sg.Radio('PERGUNTA: '+pergunta, "RD1", key='1')],
+            [sg.Radio('RESPOSTA: '+resposta, "RD1", key='2')],
+            [sg.Radio('ALTERNATIVA 1: '+alternativa1, "RD1", key='3')],
+            [sg.Radio('ALTERNATIVA 2: '+alternativa2, "RD1", key='4')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Editar Pergunta').Layout(layout)
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['4']:
+            opcao = 4
+        if button in (None, 'Cancelar'):
+            self.close()
+            opcao = "0", 0
+            return opcao
+        self.close()
+        if opcao == 1:
+            sg.ChangeLookAndFeel('Reddit')
+            layout = [
+                [sg.Text('Pergunta antiga: ' + pergunta, size=(50, 1))],
+                [sg.Text('Digite a nova pergunta:', size=(50,1))],
+                [sg.Text('Pergunta: ', size=(15, 1)), sg.InputText('', key='alteracao')],
+                [sg.Button('Confirmar')]
+
             ]
-        self.__window = sg.Window('Perguntas').Layout(layout)
+            self.__window = sg.Window('Selecione uma pergunta').Layout(layout)
+
+            button, values = self.open()
+            alteracao = values['alteracao']
+            return alteracao, opcao
+        elif opcao == 2:
+            sg.ChangeLookAndFeel('Reddit')
+            layout = [
+                [sg.Text('Resposta antiga: '+resposta, size=(50, 1))],
+                [sg.Text('Digite a nova resposta:', size=(50, 1))],
+                [sg.Text('Resposta: ', size=(15, 1)), sg.InputText('', key='alteracao')],
+                [sg.Button('Confirmar')]
+
+            ]
+            self.__window = sg.Window('Selecione uma pergunta').Layout(layout)
+
+            button, values = self.open()
+            alteracao = values['alteracao']
+            return alteracao, opcao
+        elif opcao == 3:
+            sg.ChangeLookAndFeel('Reddit')
+            layout = [
+                [sg.Text('Alternativa antiga: ' + alternativa1, size=(50, 1))],
+                [sg.Text('Digite a nova alternativa 1', size=(50, 1))],
+                [sg.Text('Alternativa: ', size=(15, 1)), sg.InputText('', key='alteracao')],
+                [sg.Button('Confirmar')]
+
+            ]
+            self.__window = sg.Window('Selecione uma pergunta').Layout(layout)
+
+            button, values = self.open()
+            alteracao = values['alteracao']
+            return alteracao, opcao
+        elif opcao == 4:
+            sg.ChangeLookAndFeel('Reddit')
+            layout = [
+                [sg.Text('Alternativa antiga: ' + alternativa2)],
+                [sg.Text('Digite a nova alternativa 2', size=(50, 1))],
+                [sg.Text('Alternativa: ', size=(15, 1)), sg.InputText('', key='alteracao')],
+                [sg.Button('Confirmar')]
+
+            ]
+            self.__window = sg.Window('Selecione uma pergunta').Layout(layout)
+
+            button, values = self.open()
+            alteracao = values['alteracao']
+            return alteracao, opcao
+
+
+    def pedir_pergunta(self):
+        sg.ChangeLookAndFeel('Reddit')
+        layout = [
+            [sg.Text('Digite o codigo da Pergunta', font=("Helvica", 15))],
+            [sg.Text('codigo: ', size=(15, 1)), sg.InputText('', key='codigo')],
+            [sg.Button('Confirmar')]
+
+        ]
+        self.__window = sg.Window('Selecione uma pergunta').Layout(layout)
+
+        button, values = self.open()
+        codigo = values['codigo']
+        self.close()
+        return codigo
+
+
+    def close(self):
+        self.__window.Close()
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
